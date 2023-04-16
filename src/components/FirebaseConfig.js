a563c0665f5274b4b492159cad4e84de56a7ff61
kienvo3105 <hvtrungkien@gmail.com>
2023-04-16 16:29:09 +0700

    connect firebase

---
+++ src/components/FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBqOf6iEW12NdtG9ohOujD4AUfzcG0kdgc",
+    authDomain: "musicapp-416e6.firebaseapp.com",
+    projectId: "musicapp-416e6",
+    storageBucket: "musicapp-416e6.appspot.com",
+    messagingSenderId: "390671815312",
+    appId: "1:390671815312:web:81c6f8bf63a1d672537e15",
+    measurementId: "G-FMLK43X27N"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
