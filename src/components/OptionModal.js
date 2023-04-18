b2d76b3d7ad9674e53c07c3312658e991da9a1af
kienvo3105 <hvtrungkien@gmail.com>
2023-04-19 02:00:51 +0700

    update OptionModal component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,7 +6,11 @@ import {
     StatusBar,
     Text,
     TouchableWithoutFeedback,
+    SafeAreaView,
+    Image,
 } from 'react-native';
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 const OptionModal = ({
     visible,
@@ -18,13 +22,19 @@ const OptionModal = ({
 }) => {
 
     return (
-        <>
+        <SafeAreaView style={styles.container}>
             <StatusBar hidden />
             <Modal animationType='slide' transparent visible={visible} statusBarTranslucent>
                 <View style={styles.modal}>
-                    <Text style={styles.title} numberOfLines={2}>
-                        {currentItem?.song}
-                    </Text>
+                    <View style={styles.header}>
+                        <Image
+                            source={require("../../assets/temp.jpg")}
+                            style={styles.poster}
+                        />
+                        <Text style={styles.title} numberOfLines={2}>
+                            {currentItem?.song}
+                        </Text>
+                    </View>
                     <View style={styles.optionContainer}>
                         {options.map(optn => {
                             return (
@@ -32,7 +42,14 @@ const OptionModal = ({
                                     key={optn.title}
                                     onPress={optn.onPress}
                                 >
-                                    <Text style={styles.option}>{optn.title}</Text>
+                                    <View style={styles.function} >
+                                        <MaterialIcons
+                                            name={optn.icon}
+                                            size={30}
+                                            color="black"
+                                        ></MaterialIcons>
+                                        <Text style={styles.option}>{optn.title}</Text>
+                                    </View>
                                 </TouchableWithoutFeedback>
                             );
                         })}
@@ -42,11 +59,14 @@ const OptionModal = ({
                     <View style={styles.modalBg} />
                 </TouchableWithoutFeedback>
             </Modal>
-        </>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     modal: {
         position: 'absolute',
         bottom: 0,
@@ -57,22 +77,36 @@ const styles = StyleSheet.create({
         zIndex: 1000,
         backgroundColor: 'white',
     },
+    header: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        borderBottomColor: "#efefef",
+        borderBottomWidth: 1,
+    },
+    poster: {
+        width: 50,
+        height: 50,
+        marginLeft: 20,
+        borderRadius: 15,
+    },
     optionContainer: {
         padding: 20,
     },
     title: {
-        fontSize: 18,
+        fontSize: 25,
         fontWeight: 'bold',
         padding: 20,
-        paddingBottom: 0,
+        paddingBottom: 20,
         color: 'black',
     },
     option: {
-        fontSize: 16,
-        fontWeight: 'bold',
+        fontSize: 18,
+        // fontWeight: 'bold',
         color: 'black',
         paddingVertical: 10,
         letterSpacing: 1,
+        paddingLeft: 20,
     },
     modalBg: {
         position: 'absolute',
@@ -81,6 +115,11 @@ const styles = StyleSheet.create({
         left: 0,
         bottom: 0,
         backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    },
+    function: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
 });
 
