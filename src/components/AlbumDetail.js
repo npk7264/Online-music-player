21ccc88f8c4f8fae855596f3775e69fdf5302af3
kienvo3105 <hvtrungkien@gmail.com>
2023-04-22 21:41:10 +0700

    creact AlbumDetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AlbumDetail = () => {
+    return (
+        <View>
+            <Text>AlbumDetail</Text>
+        </View>
+    )
+}
+
+export default AlbumDetail
+
+const styles = StyleSheet.create({})
