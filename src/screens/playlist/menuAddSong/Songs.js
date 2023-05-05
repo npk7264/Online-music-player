748ab64fb236ff41199c5f34f1aa906d12030698
kienvo3105 <hvtrungkien@gmail.com>
2023-05-06 01:48:24 +0700

    create AddSongItem component

--- src/screens/playlist/menuAddSong/Songs.js
+++ src/screens/playlist/menuAddSong/Songs.js
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AddSongItem from '../AddSongItem'
+
+
+// test data
+import { songs } from '../../../../data'
+const data = { ...songs[1] };
 
 const Songs = () => {
     return (
         <View>
-            <Text>Songs</Text>
+            <AddSongItem info={data}></AddSongItem>
         </View>
     )
 }
